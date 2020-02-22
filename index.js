ccfd9732861c8c9fc1e85fa325037ffba88c4c2e
Nigary <46726933+Nigary@users.noreply.github.com>
2020-02-22 16:42:04 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const tokenfile = require("./token.json");
 const Discord = require("discord.js");
+const bot = new Discord.Client({disableEveryone: true});
 const prefix = "!",
 
 bot.on('ready', function() {
